60b480ec137bf59d979b708f0d636089734e9bd4
Maksym <m.fomichov@gmail.com>
2017-08-01 02:19:10 +0300

    решено

--- src/latin/Main.java
+++ src/latin/Main.java
@@ -8,17 +8,19 @@ All non-letter symbols should stay on the same places:
 Use Latin alphabet only.
  */
 
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите строку в латинской расскладке:");
-        String stringLatin = scanner.nextLine();
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Введите строку в латинской расскладке:");
+//        String stringLatin = scanner.nextLine();
+        String stringLatin = "asффb jhgf jлнолгел рпаоао  лопр прa1bcd efg!h";
 
+        // выводим через пробел все слова
         for (String text : stringLatin.split(" ")) {
-            System.out.print(sortArray(text));
+            System.out.print(sortArray(text.trim()));
             System.out.print(" ");
         }
     }
@@ -26,16 +28,31 @@ public class Main {
     // переставляем символы
     private static String sortArray(String text) {
         char[] charArray = text.toCharArray();
-        char[] tempCharArray = new char[charArray.length];
-
+        //System.out.println("charArray " + charArray.length + " " + Arrays.toString(charArray));
+        ArrayList<Character> tempCharArray = new ArrayList<>();
+        // меняем местами все латинские символы
         for (int i = 0; i < charArray.length; i++) {
-            tempCharArray[i] = charArray[charArray.length - i - 1];
+            char temp = charArray[charArray.length - i - 1];
+            if (isLatinLetter(temp)) {
+                tempCharArray.add(temp);
+            }
         }
-        return new String(tempCharArray);
+        // вставляем на свои места все не латинские символы
+        for (int i = 0; i < charArray.length; i++) {
+            char temp = charArray[i];
+            if (!isLatinLetter(temp)) {
+                tempCharArray.add(i, temp);
+            }
+        }
+        // конвертируем аррейлист чаров в стринг
+        StringBuilder builder = new StringBuilder(tempCharArray.size());
+        for (Character ch : tempCharArray) {
+            builder.append(ch);
+        }
+        return builder.toString();
     }
 
-
-    // проверям на соответсвие латинскому алфавиту
+    // проверям на соответствие латинскому алфавиту
     private static boolean isLatinLetter(char c) {
         return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
     }
